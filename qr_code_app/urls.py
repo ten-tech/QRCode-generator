8d26bab7a091777976d268459e4ca95997f0f0b7
ten-tech <tenenan.thebest@gmail.com>
2025-12-25 21:51:03 +0100

    Ajout route /api/export-pdf

--- qr_code_app/urls.py
+++ qr_code_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.qr_code_generator, name='qr_code_generator'),
     path('api/preview', views.qr_preview_api, name='qr_preview_api'),
     path('api/batch', views.batch_generate, name='batch_generate'),
+    path('api/export-pdf', views.export_pdf, name='export_pdf'),
 ]  
